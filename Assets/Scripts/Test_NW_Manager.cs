8669b2fa30bfbccea9ccf52e01ad48d98667efc3
png401 <png401@gmail.com>
2026-01-29 11:10:29 +0900

    chore: paralsync + 기타 테스트를 위한 것들
    이것들은 원격에는 올리지 말기

---
+++ Assets/Scripts/Test_NW_Manager.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Test_NW_Manager : MonoBehaviourPunCallbacks
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        PhotonNetwork.JoinLobby();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 5;
+        PhotonNetwork.JoinOrCreateRoom("TestRoom",roomOptions,null);
+    }
+
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player(vision)",Vector3.zero,Quaternion.identity);
+    }
+}
+
