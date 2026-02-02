34ee5550ba8e55643ccc7b1591c4a2b319a474fb
eluda315 <michelle3hg@gmail.com>
2026-02-02 13:31:43 +0900

    test: kill system 테스트 준비

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -61,7 +61,7 @@ public class GameStateManager : MonoBehaviourPunCallbacks, IPunObservable
 
             // 1. 인게임씬에 캐릭터 생성하기 위한 코드
             Vector2 randomPos = Random.insideUnitCircle * 2.0f;
-            PhotonNetwork.Instantiate("Player(Test)", randomPos, Quaternion.identity);
+            PhotonNetwork.Instantiate("Player(Kill)", randomPos, Quaternion.identity);
 
             // 2. 상태 초기화
             Hashtable props = new Hashtable();
